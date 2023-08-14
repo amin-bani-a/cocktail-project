8118ecf6d13caffb6186db8e1cc95287001fe574
amin-bani-a <amin.baniamerian67@gmail.com>
2023-08-14 18:40:36 +0400

    setup navabar and about and error components

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum pariatur,
+        doloribus quod obcaecati eum ducimus placeat non iusto quas sit
+        recusandae suscipit eligendi quam animi odit voluptatibus nulla
+        repudiandae beatae.F
+      </p>
+    </section>
+  );
+};
+
+export default About;
